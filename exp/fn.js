31b9085eeedebeffbdb2e404f1c1f8faedc3fe7f
Joshua Kenney <joshuagkenney@gmail.com>
2020-10-06 07:07:07 -0400

    ready for testing

--- exp/fn.js
+++ exp/fn.js
@@ -13,7 +13,6 @@ function moveConfidence(event) { // function definition
   // });
   width = parseFloat(width.slice(0, -1)); // variable reassignment
     if (width >= 99) { // set to record 100 taps
-      totalConfidence = 100;
       trialComplete = 1;
       width = document.getElementById("keyBar").style.width="0%"; // reset to 0
       jsPsych.finishTrial();  
